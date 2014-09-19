f07f0d2ad51f6e2e031640250a76809717403582
Filipe Sousa <filipe@ipb.pt>
2014-09-19 17:39:55 +0100

    add prototype

---
+++ src/main/java/pt/ipb/patterns/PrototypeExample.java
@@ -0,0 +1,24 @@
+package pt.ipb.patterns;
+
+interface Prototype {
+    Object clone();
+}
+
+
+class ConcretePrototype implements Prototype {
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+public class PrototypeExample {
+    public static void main(String arg[]) {
+        ConcretePrototype obj1 = new ConcretePrototype();
+        ConcretePrototype obj2 = (ConcretePrototype)obj1.clone();
+    }
+}
