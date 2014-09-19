e5b8ddb06bc1c1065a766141acd20c14e6bc6ace
Filipe Sousa <filipe@ipb.pt>
2014-09-19 17:34:29 +0100

    add factory pattern

---
+++ src/main/java/pt/ipb/patterns/FactoryExanple.java
@@ -0,0 +1,45 @@
+package pt.ipb.patterns;
+
+interface DecodedImage {
+    String getType();
+}
+
+interface ImageReader {
+    DecodedImage getDecodedImage();
+}
+
+class GifImage implements DecodedImage {
+    @Override
+    public String getType() {
+        return "gif";
+    }
+}
+
+class JpegImage implements DecodedImage {
+    @Override
+    public String getType() {
+        return "jpeg";
+    }
+}
+
+class GifReader implements ImageReader {
+    @Override
+    public DecodedImage getDecodedImage() {
+        return new GifImage();
+    }
+}
+
+class JpegReader implements ImageReader {
+    @Override
+    public DecodedImage getDecodedImage() {
+        return new JpegImage();
+    }
+}
+
+public class FactoryExanple {
+    public static void main(String[] args) {
+        ImageReader reader = new GifReader();
+        DecodedImage decodedImage = reader.getDecodedImage();
+        System.out.println("decodedImage = " + decodedImage.getType());
+    }
+}
