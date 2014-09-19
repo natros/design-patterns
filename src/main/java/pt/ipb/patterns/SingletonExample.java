e9c7872da7e0beaee32fd2ea3b417466f389727e
Filipe Sousa <filipe@ipb.pt>
2014-09-19 17:48:44 +0100

    add singleton

---
+++ src/main/java/pt/ipb/patterns/SingletonExample.java
@@ -0,0 +1,29 @@
+package pt.ipb.patterns;
+
+/* thread-safe */
+class Singleton {
+    // Private constructor prevents instantiation from other classes
+    private Singleton() {
+        System.out.println("Singleton.Singleton");
+    }
+
+    public static Singleton getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    /**
+     * LazyHolder is loaded on the first execution of Singleton.getInstance()
+     * or the first access to LazyHolder.INSTANCE, not before.
+     */
+    private static class LazyHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+}
+
+public class SingletonExample {
+    public static void main(String[] args) {
+        System.out.println("start");
+        Singleton.getInstance();
+        System.out.println("end");
+    }
+}
